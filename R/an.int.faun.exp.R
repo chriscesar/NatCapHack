16220f13debb53265d1f29a93c55164488aba5a8
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-10-31 14:34:34 +0000

    .

--- R/an.int.faun.exp.R
+++ R/an.int.faun.exp.R
@@ -49,7 +49,7 @@ toc(log=TRUE)
 tic("load data");print("load data")
 df_abd_0 <- as_tibble(read.csv("data/abund_all.csv", header = TRUE)) # faunal abundance data
 df_traits_0 <- as_tibble(read.csv("data/species_all_traits.csv", header=TRUE)) # sp. traits
-df_sites_0 <- as_tibble(read.csv("data/sites_all.csv", header = TRUE)) # sites data
+df_sites_0 <- as_tibble(read.csv("data/sites_all_folkupdated.csv", header = TRUE)) # sites data
 df_env_0 <- as_tibble(readxl::read_xlsx("data/env_psa_all_NEW.xlsx",
                                         sheet = "env_psa_all",guess_max = 10000
                                         )) # environmental data
@@ -81,7 +81,7 @@ df_abd_0 %>%
   ## Convert to presnce/absence
   mutate(.,AbundUse = 1) %>% 
   dplyr::select(., c(Site:Mesh_Size_mm,ScientificName_accepted:Species,
-                     TaxonNameUSE,AbundUse)) %>% 
+                     TaxonNameUSE,AbundUse, tmp)) %>% 
   dplyr::select(.,-ScientificName_accepted) %>% 
   group_by(across(!AbundUse)) %>% 
   summarise(., AbundUse = sum(AbundUse),.groups = "drop") %>% 
@@ -89,15 +89,52 @@ df_abd_0 %>%
 
 ## quick ordination
 
-df_abd_1 %>% #names()
-  dplyr::select(., Site, Station, Date, Mesh_Size_mm,
-                TaxonNameUSE, AbundUse) %>% 
-  group_by(across(!AbundUse)) %>% 
-  summarise(., AbundUse = sum(AbundUse),.groups = "drop") %>% 
-  pivot_wider(.,
-              names_from = TaxonNameUSE,
-              values_from = AbundUse,
-              values_fill = list(AbundUse = 0)) %>%
-  dplyr::select(.,!c(Site:Mesh_Size_mm)) -> df_tmp_w
+# df_abd_1 %>% #names()
+#   dplyr::select(., Site, Station, Date, Mesh_Size_mm,
+#                 TaxonNameUSE, AbundUse) %>% 
+#   group_by(across(!AbundUse)) %>% 
+#   summarise(., AbundUse = sum(AbundUse),.groups = "drop") %>% 
+#   pivot_wider(.,
+#               names_from = TaxonNameUSE,
+#               values_from = AbundUse,
+#               values_fill = list(AbundUse = 0)) %>%
+#   dplyr::select(.,!c(Site:Mesh_Size_mm)) -> df_tmp_w
+# 
+# ord1 <- metaMDS(df_tmp_w)
+########################
+# Look at Plymouth data only ####
+## 01 create temporary variables
+# df_abd_1$tmp <- paste0(df_abd_1$Site,df_abd_1$Station,df_abd_1$Date)
+# df_env_0$tmp <- paste0(df_env_0$Site,df_env_0$Station,df_env_0$Date)
+# df_sites_0$tmp <- paste0(df_sites_0$Site,df_sites_0$Station,df_sites_0$Date)
 
-ord1 <- metaMDS(df_tmp_w)
+## 02 Pivot Wider
+df_abd_1 %>%
+  dplyr::select(., -c(Kingdom:Species)) %>% 
+  group_by(across(!AbundUse)) %>% 
+  summarise(AbundUse = sum(AbundUse),.groups = "drop") %>%
+  ungroup() %>% 
+  pivot_wider(., names_from = TaxonNameUSE, values_from = AbundUse,
+              values_fill = 0) -> df_abd_1_w
+  
+df_env_0 %>% 
+  dplyr::select(., tmp, PSA_Variable, Value) %>% 
+group_by(across(!Value)) %>% 
+  pivot_wider(.,values_from = PSA_Variable, values_from = Value)
+
+## 03 Append BSH codes to abundance data ####
+df_abd_1 %>% 
+  left_join(.,df_sites_0, by = "tmp") -> dfall
+  # left_join(.,df_env_0, by = "tmp") %>% 
+  # dplyr::select(., -c(Site.y,Station.y,Date.y,Site,Station,
+  #                     Easting.y,Northing.y, Water_Depth_m.y, Date)) %>% #names(.)
+  # rename(.,Site = Site.x, Station = Station.x, Easting = Easting.x,
+  #        Northing = Northing.x,Water_Depth_m = Water_Depth_m.x,
+  #        Date = Date.x)
+
+write.csv(dfall, file="data/dfall.csv", row.names = FALSE)
+
+## 02 Extract only Plymouth data ####
+dfall %>% #names(.)
+  dplyr::filter(., Site == "Plymouth") %>% 
+  dplyr::filter(.,Mud_Sand_Gravel)
